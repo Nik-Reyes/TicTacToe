82f7e8de9fde02f5841946d3764495202a009316
“Nik <nikolaspreyes@gmail.com>
2025-05-26 20:30:03 -0700

    refactor: implement logic to check for wins by column

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // GAMEBOARD IIFE //
 const gameboard = (() => {
-  let board = ["X", "X", "O", "O", "", "O", "O", "X", "X"];
+  let board = ["X", "X", "O", "X", "", "O", "X", "O", "X"];
 
   const updateBoard = (newBoard) => (board = newBoard);
   const getBoard = () => board;
@@ -42,16 +42,46 @@ const gameController = (() => {
     for (let i = 0; i < gameboard.getBoard().length; i += 3) {
       const [first, second, third] = gameboard.getBoard().slice(i, i + 3);
       if ([first, second, third].every((cell, i, row) => cell === row[0])) {
+        game.winner =
+          first === playerOne.getMarker()
+            ? playerOne.getName()
+            : playerTwo.getName();
         console.log(
           `the winner is ${
-            first === "X" ? playerOne.getName() : playerTwo.getName()
+            first === playerOne.getMarker()
+              ? playerOne.getName()
+              : playerTwo.getName()
           }`
         );
         return;
       }
     }
 
-    console.log("There is no winner");
+    for (let i = 0; i < 3; i++) {
+      const idx_1 = i;
+      const idx_2 = idx_1 + 3;
+      const idx_3 = idx_2 + 3;
+      const first = gameboard.getBoard().slice(idx_1, idx_1 + 1)[0];
+      const second = gameboard.getBoard().slice(idx_2, idx_2 + 1)[0];
+      const third = gameboard.getBoard().slice(idx_3, idx_3 + 1)[0];
+
+      if ([first, second, third].every((cell, i, col) => cell === col[0])) {
+        game.winner =
+          first === playerOne.getMarker()
+            ? playerOne.getName()
+            : playerTwo.getName();
+        console.log(
+          `the winner is ${
+            first === playerOne.getMarker()
+              ? playerOne.getName()
+              : playerTwo.getName()
+          }`
+        );
+        return;
+      }
+    }
+
+    game.isDraw = true;
 
     // if there is a winner (determine who the winner is by the marker)
     // return true
@@ -81,6 +111,7 @@ const gameController = (() => {
     game.turn = game.playerOneTurn ? game.playerOneTurn : game.playerTwoTurn;
   };
   const getTurn = () => game.turn;
+  const getWinner = () => game.winner;
 
   return {
     clearBoard,
@@ -89,13 +120,30 @@ const gameController = (() => {
     updateScore,
     updateTurn,
     getTurn,
+    getWinner,
   };
 })();
 
 console.log(gameboard.getBoard());
-
 gameController.scanForWin();
+console.log(gameController.getWinner());
 // CHECK WINNER BY COLUMN LOGIC //
-// const [first, second, third] = gameboard.getBoard().map();
+// const [first, second, third] = gameboard.getBoard().slice(0, 0 + 3);
 // const [fourth, fifth, sixth] = gameboard.getBoard().slice(3, 3 + 3);
 // const [seventh, eight, ninth] = gameboard.getBoard().slice(6, 6 + 3);
+
+// loop 3 times
+// first iteration: i = 0
+// first = i; --0
+// second = first + 3; --3
+// third = second + 3; --6
+
+// second iteration: i = 1
+// first = i; --1`
+// second = first + 3; --4
+// third = second + 3; --7
+
+// third iteration: i = 2
+// first = i; --2
+// second = first + 3; --5
+// third = second + 3; --8
